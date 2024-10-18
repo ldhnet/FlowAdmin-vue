78eabed48b1ff062a139564c56412767bcf7a4c8
lidonghui <574427343@qq.com>
2024-10-18 13:22:16 +0800

    api url update

--- src/api/mockoutside.js
+++ src/api/mockoutside.js
@@ -10,8 +10,8 @@ const headers = {
   "Userid": cache.session.get('userId'),
   "Username": cache.session.get('userName')
 }
-//let baseUrl = "http://117.72.70.166:7001";
-let baseUrl = "http://localhost:7001";
+let baseUrl = "http://117.72.70.166:7001";
+//let baseUrl = "http://localhost:7001";
 
 /**
  * 获取接入业务方列表
