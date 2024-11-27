24a487ae810673425037044b79c56486a87ea08b
ldhnet <574427343@qq.com>
2024-11-27 09:04:33 +0800

    update code

--- src/components/DynamicForm/index.vue
+++ src/components/DynamicForm/index.vue
@@ -8,23 +8,53 @@
   </template>
   
   <script setup>
-  import { ref } from 'vue'
+  import { ref, watch,  computed, onMounted  } from 'vue'
   const formDesign = ref(null) 
+
+const getWatchData = () => {
+    return formDesign.value.getFormJson(); 
+}
+onMounted(() => { 
+     let ret= getWatchData();
+     console.log('ret===========:', ret); 
+});
+
+// watch(() => getWatchData(),  (newReturnValue) => { 
+//         console.log('Method returned a new value:', newReturnValue); 
+// });
+
+
   const getData = () => {
-    let exportData= formDesign.value.getFormJson();
+    let  exportData= formDesign.value.getFormJson();
     //console.log('exportData=========',JSON.stringify(exportData)) 
     return new Promise((resolve, reject) => {
       resolve({ formData: exportData })
     })
   }
+ 
+   /*
+  获取字段
+  */
+  const getFieldList =()=> { 
+    let  exportField = formDesign.value.getFormFieldJson();
+    return new Promise((resolve, reject) => {
+      resolve({ formData: exportField.formFields })
+    })
+  } 
   defineExpose({
-    getData
+    getData,
+    getFieldList
   })
   </script>
   
   <style lang="css" scoped>
   body {
     margin: 0;  /* 如果页面出现垂直滚动条，则加入此行CSS以消除之 */
-  } 
+  }  
+  .el-dialog {
+    width: 700px !important;
+    border: 1px solid #DDE1E5 !important;
+    border-radius: 3px !important;
+  }
   </style>
   
